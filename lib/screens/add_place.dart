e566dbfed6d2686dcf552503187b96643ac487a2
parth-n <parth.nepalia@gmail.com>
2024-09-15 23:49:01 +0530

    null exception handled

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:place_x/models/place.dart';
 import 'package:place_x/providers/userprovider.dart';
 import 'package:place_x/widgets/image_input.dart';
 import 'package:place_x/widgets/location_input.dart';
@@ -16,17 +17,20 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
   File? _selectedImage;
+  PlaceLocation? _selectedLocation;
 
   void _savePlace() {
     final enteredTitle = _titleController.text;
 
-    if (enteredTitle.isEmpty || _selectedImage == null) {
+    if (enteredTitle.isEmpty ||
+        _selectedImage == null ||
+        _selectedLocation == null) {
       return;
     }
 
     ref
         .read(userPlacesProvider.notifier)
-        .addPlace(enteredTitle, _selectedImage!);
+        .addPlace(enteredTitle, _selectedImage!, _selectedLocation!);
     Navigator.of(context).pop();
   }
 
@@ -66,7 +70,9 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
             const SizedBox(
               height: 10,
             ),
-            LocationInput(),
+            LocationInput(onSelectLocation: (location) {
+              _selectedLocation = location;
+            }),
 
             const SizedBox(
               height: 16,
